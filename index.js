27bc8a4ad08ba9a65ee8f8f3068ad37f45a382ba
Tim Allen <tim@noblesamurai.com>
2020-09-02 16:12:59 +1000

    wip

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const last = require('lodash.last');
+const sanitize = require('./lib/sanitize');
+
 /**
  * Map one array of strings onto another array of strings.
  *
@@ -13,28 +16,33 @@
  * Returns false if strings cannot be mapped onto toStrings.
  */
 module.exports = function (strings, toStrings) {
-  // validate that string matches toStrings in the first place.
-  if (sanitize(strings.join(' ')) !== sanitize(toStrings.join(' '))) {
-    return false;
-  }
+  // Validate that string matches toStrings in the first place.
+  if (
+    strings.join('').replace(/\s+/g, '') !==
+    toStrings.join('').replace(/\s+/g, '')
+  ) return false;
 
-  const stringWordCounts = strings.map(function (string) {
+  const stringWordCounts = strings.map(string => {
     return splitIntoWords(string).length;
   });
-  return toStrings.reduce(function (map, toString) {
+  // eslint-disable-next-line unicorn/no-reduce
+  return toStrings.reduce((map, toString) => {
     const words = splitIntoWords(toString);
+    console.log({ words });
     let stringWordCount = stringWordCounts.shift();
     const segments = [];
     while (stringWordCounts.length && stringWordCount < words.length) {
       segments.push(stringWordCount ? words.splice(0, stringWordCount).join(' ') : '');
       stringWordCount = stringWordCounts.shift();
     }
+
     stringWordCount -= words.length;
     segments.push(words.join(' '));
-    // if words remaining, push back onto the start of the word counts list
+    // If words remaining, push back onto the start of the word counts list
+    console.log({ stringWordCount });
     if (stringWordCount) {
       stringWordCounts.unshift(stringWordCount);
-    // otherwise, check and add any empty entrys to the end of the current segment
+    // Otherwise, check and add any empty entrys to the end of the current segment
     // before we continue.
     } else {
       while (stringWordCounts.length && stringWordCounts[0] === 0) {
@@ -42,20 +50,48 @@ module.exports = function (strings, toStrings) {
         stringWordCounts.shift();
       }
     }
-    return map.concat([{ segments: segments, more: Boolean(stringWordCount) }]);
+
+    return map.concat([{ segments, more: Boolean(stringWordCount) }]);
   }, []);
 };
 
-function sanitize (text) {
-  // replace all punctuation with a space (punctuation still denotes a gap).
-  // Note: we don't want to remove ''s which are in some words in the htk dictionary.
-  return (text || '').replace(/[^\w\s']/g, ' ')
-    .replace(/\s+/g, ' ') // collapse whitespace
-    .replace(/^\s|\s$/g, ''); // and trim
-}
-
 function splitIntoWords (text) {
   text = sanitize(text);
-  if (!text.length) return []; // return empty array if empty string.
+  if (text.length === 0) return []; // Return empty array if empty string.
   return text.split(/\s+/);
 }
+
+module.exports = function (strings, toStrings) {
+  const returnValue = [];
+  for (const string2 of toStrings) {
+    const current = [''];
+    for (const c2 of string2) {
+      // Consume any spaces in c2
+      if (/\s/.test(c2)) {
+        current[current.length - 1] = last(current) + c2;
+        continue;
+      }
+
+      const string1 = strings.shift();
+      for (const c1 of string1) {
+        console.log({ c1, string1, c2, string2, current, returnValue });
+        if (/\s/.test(c1)) continue; // Consume spaces
+        if (c1 === c2) {
+          current[current.length - 1] = last(current) + c2;
+          const remainder = string1.slice(1);
+          console.log({ remainder });
+          if (remainder.length > 0) strings.unshift(remainder); // eslint-disable-line max-depth
+          break; // Next char in toString
+        }
+
+        console.log('no match');
+
+        return false;
+      }
+    }
+
+    returnValue.push({ segments: current, more: false });
+  }
+
+  return returnValue;
+};
--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 const expect = require('chai').expect;
 const map = require('..');
 
-describe('lib/map', function () {
-  it('should map correctly one list of strings onto another', function () {
+describe('lib/map', () => {
+  it.only('should map correctly one list of strings onto another', () => {
     const out = map(['a', 'b c', 'd e f'], ['a b c', 'd e', 'f']);
     expect(out).to.eql([
       { segments: ['a', 'b c'], more: false },
@@ -11,7 +11,7 @@ describe('lib/map', function () {
     ]);
   });
 
-  it('should work with an empty string at the start of the array', function () {
+  it('should work with an empty string at the start of the array', () => {
     const out = map(['', 'a', 'b c', 'd e f'], ['a b c', 'd e', 'f']);
     expect(out).to.eql([
       { segments: ['', 'a', 'b c'], more: false },
@@ -20,7 +20,7 @@ describe('lib/map', function () {
     ]);
   });
 
-  it('should work with an empty string at the end of the array', function () {
+  it('should work with an empty string at the end of the array', () => {
     const out = map(['a', 'b c', 'd e f', ''], ['a b c', 'd e', 'f']);
     expect(out).to.eql([
       { segments: ['a', 'b c'], more: false },
@@ -29,7 +29,7 @@ describe('lib/map', function () {
     ]);
   });
 
-  it('should work with a null value in the array', function () {
+  it('should work with a null value in the array', () => {
     const out = map(['a', 'b c', null, 'd e f'], ['a b c', 'd e', 'f']);
     expect(out).to.eql([
       { segments: ['a', 'b c', ''], more: false },
@@ -38,18 +38,18 @@ describe('lib/map', function () {
     ]);
   });
 
-  it('should handle differences in whitespace', function () {
+  it('should handle differences in whitespace', () => {
     const out = map(
       ['Hello Bob ...', 'You are great.'],
       ['Hello Bob', '...You are great.']
     );
     expect(out).to.eql([
-      { segments: ['Hello Bob'], more: false },
-      { segments: ['You are great'], more: false }
+      { segments: ['Hello Bob'], more: true },
+      { segments: [' ...', 'You are great.'], more: false }
     ]);
   });
 
-  it('is ok with unicode space chars', function () {
+  it('is ok with unicode space chars', () => {
     const out = map(
       ['A melhor fonte é mesmo o sol.', '\u200B e ai gostou'],
       ['A melhor fonte é mesmo o sol.\u200B', 'e ai gostou']
@@ -57,11 +57,22 @@ describe('lib/map', function () {
     expect(out).to.not.be.false();
   });
 
-  it('works on this bad case', function () {
+  it('works on this bad case', () => {
     const { sceneTexts, sentenceTexts } = require('./bad-case.json');
     const out = map(sceneTexts, sentenceTexts);
     const segments = out.map(mapping => mapping.segments).flat();
     expect(out.length).to.equal(sentenceTexts.length);
-    expect(out.reduce((acc, mapping) => acc + mapping.segments.length, 0)).to.equal(sceneTexts.length);
+    expect(segments.length).to.equal(sceneTexts.length);
+  });
+
+  it('works on this bad case (simpler)', () => {
+    const { sceneTexts, sentenceTexts } = {
+      sceneTexts: ['a', 'b', '.'],
+      sentenceTexts: ['a', 'b', '.']
+    };
+    const out = map(sceneTexts, sentenceTexts);
+    const segments = out.map(mapping => mapping.segments).flat();
+    expect(out.length).to.equal(sentenceTexts.length);
+    expect(segments.length).to.equal(sceneTexts.length);
   });
 });
